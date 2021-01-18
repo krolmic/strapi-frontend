a9fefa4ef34f4eb2dc73a6cdbb0759401ece788e
Michael Krol <michael.krol@ambient-innovation.com>
2021-01-18 19:14:09 +0100

    Add frontend code

---
+++ src/containers/Contact.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="uk-section">
+      <div class="uk-container uk-container-large">
+        <h1>A well-organized overview of ways to contact me.</h1>
+        <img class="uk-margin-medium-right" src="dog_phone.gif"/>
+        <a
+            v-for="wayToContactMe in waysToContactMe"
+            :key="wayToContactMe.label"
+            :href="wayToContactMe.url"
+            class="uk-button uk-button-primary uk-margin-small-right"
+        >
+          {{ wayToContactMe.label }}
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      api_url: process.env.VUE_APP_STRAPI_API_URL,
+      waysToContactMe: [
+        {label: 'GitHub', url: 'https://github.com/krolmic'},
+        {label: 'Twitter', url: 'https://twitter.com/krol_mic'},
+        {label: 'E-Mail', url: 'mailto: michael.krol@protonmail.com'},
+      ],
+    };
+  },
+};
+</script>
